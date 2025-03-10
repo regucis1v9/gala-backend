f0ccf9c8e4ad774d92ce85b256b9c960d0eeb0cf
Regnars Klavins <regnars.klavins@gmail.com>
2025-03-11 00:50:52 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -12,7 +12,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'], // Specify your frontend origin
+    'allowed_origins' => ['https://gala-frontend.vercel.app/'], // Specify your frontend origin
 
     'allowed_origins_patterns' => [],
 
